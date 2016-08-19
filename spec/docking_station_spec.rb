ca068d2fae534bd922c6e0d7d0b6df4f0b046a47
Johnny Dworzynski <j.dworzynski@me.com>
2016-08-19 15:05:05 +0100

    test refactoring

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,15 +3,15 @@ require 'rspec/its'
 
 describe DockingStation do
 
-let(:bike) {double:bike}
+let(:bike) {double :bike, :working? => true}
+#let{:working_bike} {double :working_bike, }
 
   it "when asking DockingStation to release_bike, a bike is released" do
     expect(subject).to respond_to(:release_bike) #WHy do we need to pass the release_bike method as a symbol
   end
 
   it "released working bikes" do
-    allow(bike).to receive(:working?).and_return(true)
-    subject.dock(bike)#NEED DOUBLE?
+    subject.dock(bike)
     expect(subject.release_bike).to be_working
   end
 
@@ -20,48 +20,38 @@ let(:bike) {double:bike}
     end
 
   it "docks the particular bike" do
-    bike = double(:bike) #NEED DOUBLE?
-    expect(subject.dock(bike)).to eq [bike] #this test only works when one bike exists
-  end
-
-  it "the particular is bike I've docked is amongst the docked bikes" do
-    bike1 = double(:bike) #NEED DOUBLE?
-    bike2 = double(:bike)
-    subject.dock(bike1)
-    expect(subject.dock(bike2)).to include bike1
+    expect(subject.dock(bike)).to include (bike)
   end
 
   describe "#release_bike" do
     it "releases a bike" do
-      bike = double(:bike, :working? => true) #NEED DOUBLE?
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
 
     it "should not release a broken bike" do
-      bike = double(:bike, :report => false, :working? => false) #NEED DOUBLE?
-      subject.dock(bike.report)
+      allow(bike).to receive(:working?).and_return(false)
+      subject.dock(bike)
       expect { subject.release_bike }.to raise_error "no working bikes available"
     end
 
     it "raises an error when there are no bikes available" do
-#because we don't dock any bikes in this test, there should be none available
+      #because we don't dock any bikes in this test, there should be none available
       expect { subject.release_bike }.to raise_error "no bikes available"
     end
   end
 
 
   describe "#dock" do
-    it "raises error when there is no more capacity" do #NEED DOUBLE?
-        DockingStation::DEFAULT_CAPACITY.times {subject.dock double(:bike)}
-      expect { subject.dock(double(:bike)) }.to raise_error "there is no capacity"
+    it "raises error when there is no more capacity" do
+        DockingStation::DEFAULT_CAPACITY.times {subject.dock bike}
+      expect { subject.dock(bike) }.to raise_error "there is no capacity"
     end
 
     it "broken bikes are still allowed to be docked" do
-      #pending "To satisfy this test, approach reporting differently eg.use Bike class"
-      bike = double(:bike) #NEED DOUBLE?
-       #dock and report broken bike
-      expect(subject.dock(bike.report)).to include bike
+      allow(bike).to receive(:working?).and_return(false)
+      #dock and report broken bike§
+      expect(subject.dock(bike)).to include bike
     end
   end
 
